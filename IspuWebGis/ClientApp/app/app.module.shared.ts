c98005e7408bc22d539fbe7baabca677ebe4c544
SSharyk <ssharyk@gmail.com>
2017-10-21 09:28:03 +0400

    Основы прямого геокодирования
    * модель данных - точка
    * компонент - точка в маршруте
    * сервис геокодирования

--- IspuWebGis/ClientApp/app/app.module.shared.ts
+++ IspuWebGis/ClientApp/app/app.module.shared.ts
@@ -7,16 +7,19 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { TasksContainerComponent } from './components/tasks-container/tasks-container.component';
 import { PointsContainerComponent } from './components/points-container/points-container.component';
+import { PointItemComponent } from './components/point-item/point-item.component';
 import { EsriLoaderModule } from 'angular-esri-loader';
 import { EsriMapService } from './components/esri-map/esri-map.service';
 import { EsriMapComponent } from './components/esri-map/esri-map.component';
+import { GeocoderService } from './services/geocoder.service';
 
 @NgModule({
     declarations: [
         AppComponent,
         EsriMapComponent,
         TasksContainerComponent,
-        PointsContainerComponent
+        PointsContainerComponent,
+        PointItemComponent
     ],
     imports: [
         CommonModule,
@@ -29,7 +32,8 @@ import { EsriMapComponent } from './components/esri-map/esri-map.component';
         ])
     ],
     providers: [
-        EsriMapService
+        EsriMapService,
+        GeocoderService
     ]
 })
 export class AppModuleShared {
---
+++ IspuWebGis/ClientApp/app/components/point-item/point-item.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, Input, OnInit } from '@angular/core';
+
+import { GeocoderService } from "../../services/geocoder.service";
+import { Point } from "../../classes/point";
+import { GeocodeParams } from "../../classes/geocode-params";
+
+@Component({
+    selector: 'point-item',
+    templateUrl: './point-item.component.html',
+    styleUrls: ['./point-item.component.css']
+})
+export class PointItemComponent implements OnInit {
+    @Input() point: Point;
+
+    constructor(private _geocodeService: GeocoderService) {    
+    }
+
+    ngOnInit() {
+    }
+
+    solveGeocoding() {
+        let params = new GeocodeParams(this.point.address);
+        this._geocodeService.getGeocode(params).subscribe((resp: any) => {
+            let res = JSON.parse(resp["_body"]);
+
+            /// TODO: do real business
+        });
+    }
+}
